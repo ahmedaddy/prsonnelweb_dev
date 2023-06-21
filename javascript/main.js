170ea91307afeceabf61307435a7b82d6ebec83d
Ahmed <ahmedzeintsu@gmail.com>
2023-06-21 20:44:47 +0100

    upload the design

---
+++ javascript/main.js
@@ -0,0 +1,30 @@
+// let barsIcon = document.querySelector(".bars-icon");
+// let headerNav = document.querySelector(".header-nav");
+
+// // barsIcon.onclick = function () {
+// //   headerNav.style = 'display: block;'
+// // }
+
+// barsIcon.onclick = function () {
+//   if (headerNav.classList) {
+//     headerNav.classList.toggle("d-block");
+//   }
+// };
+// let video = document.querySelector(".video");
+// let videoPauseimg = document.querySelector(".videocontainer img");
+
+// video.addEventListener("click", () => {
+//   video.classList.toggle("active");
+//   if (video.classList.contains("active")) {
+//     videoPauseimg.style.display = "none";
+//     // videoPauseimg.style.opacity = "0";
+//     video.play();
+//   } else {
+//     videoPauseimg.style.display = "block";
+//     // videoPauseimg.style.opacity = "1";
+//     video.pause();
+//   }
+// });
+// // video.onclick(function () {
+// //   video.pause();
+// // });
